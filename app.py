4a83d018ffbc165249a0a4d4b52de0eb19b97ff4
berdener <b.erdener41@gmail.com>
2025-08-16 15:50:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -437,7 +437,6 @@ def add_customer_page():
         return redirect(url_for("customers_page"))
     return render_template("add_customer.html")
 
-rows = result.mappings().all()
 @app.route("/customers/<int:customer_id>")
 def customer_detail(customer_id):
     c = Customer.query.get_or_404(customer_id)
